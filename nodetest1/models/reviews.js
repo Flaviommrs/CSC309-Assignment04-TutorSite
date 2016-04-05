5f475b23a3aab03a2492746f214a06f8b3af2c85
FeHioe <fe.hioe@gmail.com>
2016-04-05 15:49:21 -0400

    removed likes from reviews

--- nodetest1/models/reviews.js
+++ nodetest1/models/reviews.js
@@ -3,7 +3,6 @@ var mongoose = require('mongoose');
 var reviewSchema = new mongoose.Schema({
   reviewee : {type: String, required: true},
   reviewer : {type: String, required: true},
-  likes : Number,
   rating : Number,
   commented : String
 });
