6e55604e9cc09f07d783ddf9430df0ba6ade9c3d
FeHioe <fe.hioe@gmail.com>
2016-04-05 15:41:18 -0400

    review db inserts all values but likes

--- nodetest1/models/reviews.js
+++ nodetest1/models/reviews.js
@@ -2,12 +2,10 @@ var mongoose = require('mongoose');
 
 var reviewSchema = new mongoose.Schema({
   reviewee : {type: String, required: true},
-  reviews : [{
-    reviewer : String,
-    likes : Number,
-    Rating : Number,
-    commented : String
-  }]
+  reviewer : {type: String, required: true},
+  likes : Number,
+  rating : Number,
+  commented : String
 });
 
 var Review = mongoose.model('Review', reviewSchema);
