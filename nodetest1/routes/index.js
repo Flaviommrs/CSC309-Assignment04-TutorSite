f08307a3194eebcd881f9d27611d241d3adf1433
Flavio Matheus Muniz Ribeiro da Silva <FlavioMatheus@MacBook-Pro-de-Flavio-2.local>
2016-04-06 17:30:32 -0400

    adding comments

--- nodetest1/routes/index.js
+++ nodetest1/routes/index.js
@@ -130,12 +130,12 @@ router.post('/facebookSignUp',function (req, res, next){
 
   res.redirect('/fbsignup&username=' + username);
 });
-router.get('/fbsignup&username=*', function (req, res, next){
 
+/*code resposible for redirecting the user to the next step in the sign up using facebook*/
+router.get('/fbsignup&username=*', function (req, res, next){
 
   var username = decodeURIComponent(req.url.substring(19));
   console.log(username);
-
 
   res.render('fbsignup.html', {username: username});
 
@@ -520,11 +520,6 @@ router.get('/editProfile', function(req, res, next) {
     }
 });
 
-/* GET fb login page. Test*/
-router.get('/facebookLog', function(req, res, next) {
-    res.render('facebookLog.html', {});
-});
-
 /* GET user homepage page. */
 router.get('/homepage', function(req, res, next) {
     var secret = 'tutorMeSecretString';
@@ -617,7 +612,7 @@ router.get('/message', function(req, res, next) {
     }
 });
 
-/*Get message page from the logged user*/
+/*Get message page from the logged user. Username is used to indentify to whom the user wants to sent the message*/
 router.get('/message&username=*', function(req, res, next) {
 
     var secret = 'tutorMeSecretString';
@@ -1030,23 +1025,19 @@ function checkNewUserData(data)
 io.on('connection', function(client){
   console.log('a user connected');
 
+  //when the user finishes the last step in creating the account using facebook
+  //the page sends to the server the new information and the server updates the 
+  //the user information
   client.on('fb', function(data){
 
     data = JSON.parse(data);
     console.log("THE DATA IS EQUAL TO: ");
     console.log(data);
 
-    // if(!checkNewUserData(data))
-    // {
-    //     console.log("Invalid user data");
-    //     client.emit('invalidData', "-1");
-    //     return;
-    // }
-
     User.findOne({username: data["username"]}, function(err, user) {
 
       if (user)
-      {//Username not taken
+      {
         console.log("it is in ");
         var tutorB = data["type_of_user"] == "Yes" ? true : false;
         var adminB = false;
@@ -1179,7 +1170,9 @@ io.on('connection', function(client){
         });
     });
 
-
+  //when the user wants to send a message to another user it sends to the server
+  //a message asking for the room that the message will be sent. If this user never
+  //sent a message to the receiver, a new room is alocated and saved for both users.
   client.on('subscribe', function(data){
 
     User.findOne({username: data.user}, function(err, user) {
@@ -1204,6 +1197,7 @@ io.on('connection', function(client){
 
         console.log("index",index)
 
+        //checking to see if a chat room for both users already exists
         if(index < 0){
 
           Chat.count({},function(err, c){
@@ -1281,12 +1275,15 @@ io.on('connection', function(client){
 
   });
 
+  //message received by server containing the message contents and the sender.
+  //Everyone that is subscribed to the room will receive the message.
   client.on('message', function(data){
         console.log(data);
         io.sockets.in(data.room).emit('message', data);
 
         Chat.findOne({roomName: data.room}, function(err, chat){
 
+          //saving the message to the database
           var addmsg = [];
           if(chat.messages){
             var addMsg = chat.messages.slice(0);
