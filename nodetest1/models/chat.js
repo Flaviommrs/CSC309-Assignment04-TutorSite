37b245df893cdbc17d9b541a7528431e331394d8
RodrigoLGuimaraes <rolongui@yahoo.com.br>
2016-04-06 17:31:57 -0400

    small documentation on models

--- nodetest1/models/chat.js
+++ nodetest1/models/chat.js
@@ -1,7 +1,11 @@
 var mongoose = require('mongoose');
 
+//Schema for the chat
 var chatSchema = new mongoose.Schema({
 
+  //each pair of users is linked by a unique roomName, where all the messages are stored, with the sender.
+  //While our design supports only two users per room, it is possible to use this DB schema for more
+  //than two users per room.
   roomName: {type: Number, unique: true},
   messages: [{msg:String, sender:String}]
 
